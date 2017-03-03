ec586c105874b91706cfd3d05c69bb14b6f4a945
Dallas Hall <dallas.hall@gmail.com>
2017-03-03 09:27:02 -0800

    DRYing it up

--- w6d5/skeleton/react.jsx
+++ w6d5/skeleton/react.jsx
@@ -8,14 +8,10 @@ class Calculator extends React.Component {
       num1: "",
       num2: ""
     }
-    this.setNum1 = this.setNum1.bind(this);
+    this.setNum = this.setNum.bind(this);
   }
 
-  setNum1(key) {
-    return (event => this.setState({[key]: event.currentTarget.value}));
-  }
-
-  setNum2(key) {
+  setNum(key) {
     return (event => this.setState({[key]: event.currentTarget.value}));
   }
 
@@ -37,8 +33,8 @@ class Calculator extends React.Component {
     return (
       <div>
         <h1>{ this.state.result }</h1>
-        <input type="text" onChange={ this.setNum1('num1') } value={ this.state.num1 } id="num1"></input>
-        <input type="text" onChange={ this.setNum2('num2') } value={ this.state.num2 } id="num2"></input>
+        <input type="text" onChange={ this.setNum('num1') } value={ this.state.num1 } id="num1"></input>
+        <input type="text" onChange={ this.setNum('num2') } value={ this.state.num2 } id="num2"></input>
         <button onClick={ this.setResult('add')} value="add">add</button>
         <button onClick={ this.setResult('divide')} value="divide">divide</button>
         <button onClick={ this.setResult('times')} value="times">times</button>
